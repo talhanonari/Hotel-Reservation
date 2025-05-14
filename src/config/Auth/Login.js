88ece633fbcdd7be81126fc0b685c674ae529974
Muhammad Talha <muhammad.talha@burntbrains.com>
2025-05-15 00:40:00 +0500

    login update

--- src/config/Auth/Login.js
+++ src/config/Auth/Login.js
@@ -1,18 +1,23 @@
-import {loginRequest } from '../AxiosRoutes/index';
+import axios from 'axios';
 
 export const loginAndStoreToken = async () => {
   try {
-    const response = await loginRequest('/Jwt/v2/Authenticate', {
-    "username": "cat.wickets+api@resdiary.com",
-    "password": "yZ/&J[!tGKIt[9Ke+[g/sfQ#3h|l8K"
-});
-    const { Token, TokenExpiryUtc } = response;
+  const response = await axios.post(
+    `${process.env.REACT_APP_BASE_URL}/Jwt/v2/Authenticate`,
+    {
+      username: 'cat.wickets+api@resdiary.com',
+      password: 'yZ/&J[!tGKIt[9Ke+[g/sfQ#3h|l8K',
+    }
+  );
 
-    localStorage.setItem('token', Token);
-    localStorage.setItem('token_expiry', TokenExpiryUtc);
-    return Token;
-  } catch (error) {
-    console.error('Login failed:', error);
-    throw error;
-  }
+  const { Token, TokenExpiryUtc } = response.data;
+
+  localStorage.setItem('token', Token);
+  localStorage.setItem('token_expiry', TokenExpiryUtc);
+  return Token;
+} catch (error) {
+  console.error('Login failed:', error);
+  throw error;
+}
+
 };
